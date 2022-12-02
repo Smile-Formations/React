3701bafe88e6fcc56bb98748caf4ffc23d4bd105
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-12-02 09:56:32 +0100

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import { createRoot } from 'react-dom/client';
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from "./versions/v1/components/App/App";
 
-//const element = <div>Hell Yeah!</div>;
 const root = createRoot(document.getElementById('root'));
-//root.render(element);
 root.render(
     <React.StrictMode>
         <App />
