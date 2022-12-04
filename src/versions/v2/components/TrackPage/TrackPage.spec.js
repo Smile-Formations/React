6b488aa8c3504f9257891890e17638ff59a37603
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-12-04 20:30:19 +0100

    Add solution 2

--- src/versions/v2/components/TrackPage/TrackPage.spec.js
+++ src/versions/v2/components/TrackPage/TrackPage.spec.js
@@ -21,11 +21,15 @@ const renderWithRouter = (ui, path = "/", route = "/") => {
 describe("TrackPage component", () => {
   beforeEach(() => {
     trackService.getTrack = jest.fn().mockReturnValue(Promise.resolve({
-      id: 1,
-      title: "Track 1",
-      category: 1,
-      published: true,
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "id": 1,
+      "title": "Find The Real",
+      "album": "One Day Remains",
+      "band": "Alter Bridge",
+      "year": "2004",
+      "poster": "https://i.ebayimg.com/images/g/OqoAAOSwJYVg1y6E/s-l500.jpg",
+      "url": "https://www.youtube.com/watch?v=w9LZ0OkdxGg&ab_channel=AlterBridge-Topic",
+      "category": 3,
+      "description": "One Day Remains is the debut studio album by the American hard rock band Alter Bridge, released on August 10, 2004, on Wind-up Records."
     }));
   });
 
