607ad264919d6974e549e2839dd74d452ca1bd56
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-12-05 15:16:56 +0100

    Main

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,16 @@
 import List from "../List/List";
 import "./App.css"
+import {useEffect, useState} from "react";
 
 function App() {
 
-    const tracks = [
-        /* Add some tracks */
-    ];
+    const [tracks, setTracks] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/tracks')
+            .then(data => data.json())
+            .then(tracks => setTracks(tracks));
+    }, [setTracks]);
 
     return (
         <List tracks={tracks} />
