c20a70d22d0614203f564307f62374502e941cac
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-12-05 08:40:01 +0100

    Add CSS solution 2

---
+++ src/versions/v1/components/App/App.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+import App from './App';
+
+jest.mock('../../services/track/track');
+jest.mock('../../services/category/category');
+
+describe('App component', () => {
+  it('renders without crashing', async () => {
+    render(<App />);
+    const elements = await screen.findAllByText(/Track [0-9]/);
+    expect(elements.length).toBe(3);
+  });
+
+  it('filters the articles', async () => {
+    render(<App />);
+    fireEvent.click(screen.getByLabelText('Band'));
+    const elements = await screen.findAllByText(/Track [0-9]/);
+    expect(elements.length).toBe(2);
+  });
+});
+
