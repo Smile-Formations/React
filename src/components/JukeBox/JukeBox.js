217c81054f9251a63481d5cf01c14fd0114ce0a4
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-12-05 12:44:43 +0100

    Main

--- src/components/JukeBox/JukeBox.js
+++ src/components/JukeBox/JukeBox.js
@@ -1,11 +1,18 @@
+import classnames from 'classnames';
 import "./JukeBox.css"
+import {useState} from "react";
 
 function JukeBox(props) {
 
+    const [selected, setSelected] = useState(false);
     const { track } = props;
 
+    function handleClick() {
+        setSelected(prevState => !prevState);
+    }
+
     return (
-        <div className="JukeBox">
+        <div className={classnames('JukeBox', {isSelected: selected})} onClick={handleClick}>
             <div>{track.title}</div>
             <div>{track.category}</div>
         </div>
