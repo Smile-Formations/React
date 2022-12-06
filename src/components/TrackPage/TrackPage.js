917764cd8f343b74d84023eb83220314d7b3e6fa
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-12-06 14:36:18 +0100

    version 3

--- src/components/TrackPage/TrackPage.js
+++ src/components/TrackPage/TrackPage.js
@@ -11,39 +11,37 @@ import Title from '../Title/Title';
 const linkProps = { children: 'Back', to: '/' };
 
 function TrackPage() {
-  const navigate = useNavigate();
-  const { id } = useParams();
-  const [track, setTrack] = useTrack(id);
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const [track, setTrack] = useTrack(id);
 
-  const handleTrackChange = useCallback((name, value) =>
-          setTrack(prevState => ({
-            ...prevState,
-            [name]: value
-          }))
-      , [setTrack]);
+    const handleTrackChange = useCallback((name, value) =>
+            setTrack(prevState => ({
+                ...prevState,
+                [name]: value
+            }))
+        , [setTrack]);
 
-  const handleSubmit = useCallback(() =>
-          id
-              ? updateTrack(track)
-              : addTrack(track).then(track => navigate(`/track/${track.id}`))
-      , [track, id, navigate]);
+    const handleSubmit = useCallback(() =>
+            id
+                ? updateTrack(track)
+                : addTrack(track).then(track => navigate(`/track/${track.id}`))
+        , [track, id, navigate]);
 
-  const title = id
-      ? `Edit track (${id})`
-      : 'Add new track';
+    const title = id
+        ? `Edit track (${id})`
+        : 'Add new track';
 
-  return (
-      <>
-        <Title linkProps={linkProps} title={title} />
+    return (
         <Container>
-          <TrackForm
-              track={track}
-              onTrackChange={handleTrackChange}
-              onSubmit={handleSubmit}
-          />
+            <Title linkProps={linkProps} title={title} />
+            <TrackForm
+                track={track}
+                onTrackChange={handleTrackChange}
+                onSubmit={handleSubmit}
+            />
         </Container>
-      </>
-  );
+    );
 }
 
 export default TrackPage;
--- src/components/TracksPage/TracksPage.js
+++ src/components/TracksPage/TracksPage.js
@@ -12,46 +12,44 @@ const title = 'My Radio';
 const linkProps = { children: 'Add new track', to: '/track' };
 
 function TracksPage() {
-  const { tracks, filters, setTracks, setFilters } = useFilteredTracks();
+    const { tracks, filters, setTracks, setFilters } = useFilteredTracks();
 
-  const handleFilterChanged = useCallback((filter, value) =>
-          setFilters((prevState) => ({
-            ...prevState,
-            [filter]: value,
-          }))
-      , [setFilters]);
+    const handleFilterChanged = useCallback((filter, value) =>
+            setFilters((prevState) => ({
+                ...prevState,
+                [filter]: value,
+            }))
+        , [setFilters]);
 
-  const handleRemove = useCallback((id) => {
-    // Naive (ensure new list is sync with BE)
-    // removeTrack(id)
-    //   .then(() => getTracks())
-    //   .then(setTracks);
+    const handleRemove = useCallback((id) => {
+        // Naive (ensure new list is sync with BE)
+        // removeTrack(id)
+        //   .then(() => getTracks())
+        //   .then(setTracks);
 
-    // Optimistic (good when BE is slow)
-    // setTracks(prevState => prevState.filter(track => track.id !== id));
-    // removeTrack(id)
-    //   .catch(() => getTracks().then(setTracks));
+        // Optimistic (good when BE is slow)
+        // setTracks(prevState => prevState.filter(track => track.id !== id));
+        // removeTrack(id)
+        //   .catch(() => getTracks().then(setTracks));
 
-    // Classic
-    removeTrack(id)
-        .then(() => setTracks(prevState => prevState.filter(track => track.id !== id)));
-  }, [setTracks]);
+        // Classic
+        removeTrack(id)
+            .then(() => setTracks(prevState => prevState.filter(track => track.id !== id)));
+    }, [setTracks]);
 
-  return (
-      <>
-        <Title linkProps={linkProps} title={title} />
+    return (
         <Container>
-          <Filters
-              filters={filters}
-              onFilterChanged={handleFilterChanged}
-          />
-          <List
-              tracks={tracks}
-              onRemove={handleRemove}
-          />
+            <Title linkProps={linkProps} title={title} />
+            <Filters
+                filters={filters}
+                onFilterChanged={handleFilterChanged}
+            />
+            <List
+                tracks={tracks}
+                onRemove={handleRemove}
+            />
         </Container>
-      </>
-  );
+    );
 }
 
 export default TracksPage;
