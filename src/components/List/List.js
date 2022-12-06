6c07d7095594b2c9d4f67f98f87348040ef9a0bf
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-12-06 17:58:03 +0100

    main

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,13 +3,14 @@ import JukeBox from '../JukeBox/JukeBox';
 import './List.css';
 
 function List(props) {
-  const { tracks, onRemove } = props;
+  const { tracks, categories, onRemove } = props;
   return (
       <div className="List">
         {tracks.map(track => (
             <JukeBox
                 track={track}
                 key={track.id}
+                categories={categories}
                 onRemove={onRemove}
             />
         ))}
