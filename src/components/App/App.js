7c182e134ec538448899f9cf9855e062684844ee
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-12-07 13:06:27 +0100

    version 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    /*Navigate, */
+    Route,
+    Routes
+} from 'react-router-dom';
 
 import { useCategories } from '../../hooks/useCategories/useCategories';
 
